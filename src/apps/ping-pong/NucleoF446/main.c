5dae0e67654a5b7eacb51d0a0d78b1b51fd4917b
Unknown <davidzwa@gmail.com>
2021-07-08 09:47:55 +0200

    Go into looped sending mode

--- src/apps/ping-pong/NucleoF446/main.c
+++ src/apps/ping-pong/NucleoF446/main.c
@@ -28,79 +28,79 @@
 #include "timer.h"
 #include "radio.h"
 
-#if defined( REGION_AS923 )
+#if defined(REGION_AS923)
 
-#define RF_FREQUENCY                                923000000 // Hz
+#define RF_FREQUENCY 923000000 // Hz
 
-#elif defined( REGION_AU915 )
+#elif defined(REGION_AU915)
 
-#define RF_FREQUENCY                                915000000 // Hz
+#define RF_FREQUENCY 915000000 // Hz
 
-#elif defined( REGION_CN470 )
+#elif defined(REGION_CN470)
 
-#define RF_FREQUENCY                                470000000 // Hz
+#define RF_FREQUENCY 470000000 // Hz
 
-#elif defined( REGION_CN779 )
+#elif defined(REGION_CN779)
 
-#define RF_FREQUENCY                                779000000 // Hz
+#define RF_FREQUENCY 779000000 // Hz
 
-#elif defined( REGION_EU433 )
+#elif defined(REGION_EU433)
 
-#define RF_FREQUENCY                                433000000 // Hz
+#define RF_FREQUENCY 433000000 // Hz
 
-#elif defined( REGION_EU868 )
+#elif defined(REGION_EU868)
 
-#define RF_FREQUENCY                                868000000 // Hz
+#define RF_FREQUENCY 868000000 // Hz
 
-#elif defined( REGION_KR920 )
+#elif defined(REGION_KR920)
 
-#define RF_FREQUENCY                                920000000 // Hz
+#define RF_FREQUENCY 920000000 // Hz
 
-#elif defined( REGION_IN865 )
+#elif defined(REGION_IN865)
 
-#define RF_FREQUENCY                                865000000 // Hz
+#define RF_FREQUENCY 865000000 // Hz
 
-#elif defined( REGION_US915 )
+#elif defined(REGION_US915)
 
-#define RF_FREQUENCY                                915000000 // Hz
+#define RF_FREQUENCY 915000000 // Hz
 
-#elif defined( REGION_RU864 )
+#elif defined(REGION_RU864)
 
-#define RF_FREQUENCY                                864000000 // Hz
+#define RF_FREQUENCY 864000000 // Hz
 
 #else
-    #error "Please define a frequency band in the compiler options."
+#error "Please define a frequency band in the compiler options."
 #endif
 
-#define TX_OUTPUT_POWER                             14        // dBm
+#define TX_OUTPUT_POWER 14 // dBm
 
-#if defined( USE_MODEM_LORA )
+#if defined(USE_MODEM_LORA)
 
-#define LORA_BANDWIDTH                              0         // [0: 125 kHz,
-                                                              //  1: 250 kHz,
-                                                              //  2: 500 kHz,
-                                                              //  3: Reserved]
-#define LORA_SPREADING_FACTOR                       7         // [SF7..SF12]
-#define LORA_CODINGRATE                             1         // [1: 4/5,
-                                                              //  2: 4/6,
-                                                              //  3: 4/7,
-                                                              //  4: 4/8]
-#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
-#define LORA_SYMBOL_TIMEOUT                         5         // Symbols
-#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
-#define LORA_IQ_INVERSION_ON                        false
+#define LORA_BANDWIDTH 0        // [0: 125 kHz, \
+                                //  1: 250 kHz, \
+                                //  2: 500 kHz, \
+                                //  3: Reserved]
+#define LORA_SPREADING_FACTOR 7 // [SF7..SF12]
+#define LORA_CODINGRATE 2       // [1: 4/5, \
+                                //  2: 4/6, \
+                                //  3: 4/7, \
+                                //  4: 4/8]
+#define LORA_PREAMBLE_LENGTH 8  // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT 5   // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON false
+#define LORA_IQ_INVERSION_ON false
 
-#elif defined( USE_MODEM_FSK )
+#elif defined(USE_MODEM_FSK)
 
-#define FSK_FDEV                                    25000     // Hz
-#define FSK_DATARATE                                50000     // bps
-#define FSK_BANDWIDTH                               50000     // Hz
-#define FSK_AFC_BANDWIDTH                           83333     // Hz
-#define FSK_PREAMBLE_LENGTH                         5         // Same for Tx and Rx
-#define FSK_FIX_LENGTH_PAYLOAD_ON                   false
+#define FSK_FDEV 25000          // Hz
+#define FSK_DATARATE 50000      // bps
+#define FSK_BANDWIDTH 50000     // Hz
+#define FSK_AFC_BANDWIDTH 83333 // Hz
+#define FSK_PREAMBLE_LENGTH 5   // Same for Tx and Rx
+#define FSK_FIX_LENGTH_PAYLOAD_ON false
 
 #else
-    #error "Please define a modem in the compiler options."
+#error "Please define a modem in the compiler options."
 #endif
 
 typedef enum
@@ -111,10 +111,10 @@ typedef enum
     RX_ERROR,
     TX,
     TX_TIMEOUT,
-}States_t;
+} States_t;
 
-#define RX_TIMEOUT_VALUE                            1
-#define BUFFER_SIZE                                 64 // Define the payload size here
+#define RX_TIMEOUT_VALUE 1
+#define BUFFER_SIZE 64 // Define the payload size here
 
 const uint8_t PingMsg[] = "PING";
 const uint8_t PongMsg[] = "PONG";
@@ -141,59 +141,58 @@ extern Gpio_t Led2;
 /*!
  * \brief Function to be executed on Radio Tx Done event
  */
-void OnTxDone( void );
+void OnTxDone(void);
 
 /*!
  * \brief Function to be executed on Radio Rx Done event
  */
-void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr );
+void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr);
 
 /*!
  * \brief Function executed on Radio Tx Timeout event
  */
-void OnTxTimeout( void );
+void OnTxTimeout(void);
 
 /*!
  * \brief Function executed on Radio Rx Timeout event
  */
-void OnRxTimeout( void );
+void OnRxTimeout(void);
 
 /*!
  * \brief Function executed on Radio Rx Error event
  */
-void OnRxError( void );
+void OnRxError(void);
 
-#define FIRMWARE_VERSION                            0x01020000 // 1.2.0.0
+#define FIRMWARE_VERSION 0x01020000 // 1.2.0.0
 
-#define GITHUB_VERSION                              0x05000000 // 5.0.0.0
+#define GITHUB_VERSION 0x05000000 // 5.0.0.0
 
-void DisplayAppInfo( const char* appName, const Version_t* appVersion, const Version_t* gitHubVersion )
+void DisplayAppInfo(const char *appName, const Version_t *appVersion, const Version_t *gitHubVersion)
 {
-    printf( "\n###### ===================================== ######\n\n" );
-    printf( "Application name   : %s\n", appName );
-    printf( "Application version: %d.%d.%d\n", appVersion->Fields.Major, appVersion->Fields.Minor, appVersion->Fields.Patch );
-    printf( "GitHub base version: %d.%d.%d\n", gitHubVersion->Fields.Major, gitHubVersion->Fields.Minor, gitHubVersion->Fields.Patch );
-    printf( "\n###### ===================================== ######\n\n" );
+    printf("\n###### ===================================== ######\n\n");
+    printf("Application name   : %s\n", appName);
+    printf("Application version: %d.%d.%d\n", appVersion->Fields.Major, appVersion->Fields.Minor, appVersion->Fields.Patch);
+    printf("GitHub base version: %d.%d.%d\n", gitHubVersion->Fields.Major, gitHubVersion->Fields.Minor, gitHubVersion->Fields.Patch);
+    printf("\n###### ===================================== ######\n\n");
 }
-
 
 /**
  * Main application entry point.
  */
-int main( void )
+int main(void)
 {
     bool isMaster = true;
     uint8_t i;
 
     // Target board initialization
-    BoardInitMcu( );
-    BoardInitPeriph( );
-    
-    const Version_t appVersion = { .Value = FIRMWARE_VERSION };
-    const Version_t gitHubVersion = { .Value = GITHUB_VERSION };
-    DisplayAppInfo( "pingh-pongh", 
-                    &appVersion,
-                    &gitHubVersion );
+    BoardInitMcu();
+    BoardInitPeriph();
+
+    const Version_t appVersion = {.Value = FIRMWARE_VERSION};
+    const Version_t gitHubVersion = {.Value = GITHUB_VERSION};
+    DisplayAppInfo("pingh-pongh",
+                   &appVersion,
+                   &gitHubVersion);
 
     printf("Radio initializing\n");
 
@@ -204,15 +203,15 @@ int main( void )
     RadioEvents.RxTimeout = OnRxTimeout;
     RadioEvents.RxError = OnRxError;
 
-    Radio.Init( &RadioEvents );
+    Radio.Init(&RadioEvents);
 
-    const RadioState_t state = Radio.GetStatus();
+    RadioState_t state = Radio.GetStatus();
     printf("Radio state %d", state);
 
     printf("Radio init done\n");
 
     state = Radio.GetStatus();
-    Radio.SetChannel( RF_FREQUENCY );
+    Radio.SetChannel(RF_FREQUENCY);
 
     printf("Radio state %d", state);
 
@@ -220,58 +219,76 @@ int main( void )
 
     printf("Radio state %d", state);
 
-#if defined( USE_MODEM_LORA )
+#if defined(USE_MODEM_LORA)
 
-    Radio.SetTxConfig( MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
-                                   LORA_SPREADING_FACTOR, LORA_CODINGRATE,
-                                   LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
-                                   true, 0, 0, LORA_IQ_INVERSION_ON, 3000 );
+    Radio.SetTxConfig(MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
+                      LORA_SPREADING_FACTOR, LORA_CODINGRATE,
+                      LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
+                      true, 0, 0, LORA_IQ_INVERSION_ON, 3000);
 
-    Radio.SetRxConfig( MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
-                                   LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
-                                   LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
-                                   0, true, 0, 0, LORA_IQ_INVERSION_ON, true );
+    Radio.SetRxConfig(MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
+                      LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
+                      LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
+                      0, true, 0, 0, LORA_IQ_INVERSION_ON, true);
 
-    Radio.SetMaxPayloadLength( MODEM_LORA, BUFFER_SIZE );
+    Radio.SetMaxPayloadLength(MODEM_LORA, BUFFER_SIZE);
 
-#elif defined( USE_MODEM_FSK )
+#elif defined(USE_MODEM_FSK)
 
-    Radio.SetTxConfig( MODEM_FSK, TX_OUTPUT_POWER, FSK_FDEV, 0,
-                                  FSK_DATARATE, 0,
-                                  FSK_PREAMBLE_LENGTH, FSK_FIX_LENGTH_PAYLOAD_ON,
-                                  true, 0, 0, 0, 3000 );
+    Radio.SetTxConfig(MODEM_FSK, TX_OUTPUT_POWER, FSK_FDEV, 0,
+                      FSK_DATARATE, 0,
+                      FSK_PREAMBLE_LENGTH, FSK_FIX_LENGTH_PAYLOAD_ON,
+                      true, 0, 0, 0, 3000);
 
-    Radio.SetRxConfig( MODEM_FSK, FSK_BANDWIDTH, FSK_DATARATE,
-                                  0, FSK_AFC_BANDWIDTH, FSK_PREAMBLE_LENGTH,
-                                  0, FSK_FIX_LENGTH_PAYLOAD_ON, 0, true,
-                                  0, 0,false, true );
+    Radio.SetRxConfig(MODEM_FSK, FSK_BANDWIDTH, FSK_DATARATE,
+                      0, FSK_AFC_BANDWIDTH, FSK_PREAMBLE_LENGTH,
+                      0, FSK_FIX_LENGTH_PAYLOAD_ON, 0, true,
+                      0, 0, false, true);
 
-    Radio.SetMaxPayloadLength( MODEM_FSK, BUFFER_SIZE );
+    Radio.SetMaxPayloadLength(MODEM_FSK, BUFFER_SIZE);
 
 #else
-    #error "Please define a frequency band in the compiler options."
+#error "Please define a frequency band in the compiler options."
 #endif
-
 
     printf("Radio started. Listening\n");
 
-    Radio.Rx( RX_TIMEOUT_VALUE );
+    while (1)
+    {
+        // Send the next PING frame
+        Buffer[0] = 'P';
+        Buffer[1] = 'I';
+        Buffer[2] = 'N';
+        Buffer[3] = 'G';
+        // We fill the buffer with numbers for the payload
+        for (i = 4; i < BufferSize; i++)
+        {
+            Buffer[i] = i - 4;
+        }
+        
+        Radio.Send(Buffer, BufferSize);
+        DelayMs(1000);
+        printf("PINGED\n\r");
+        DelayMs(100);
+    }
+
+    Radio.Rx(RX_TIMEOUT_VALUE);
 
     printf("Radio going into ping-pong mode.\n");
 
-    while( 1 )
+    while (1)
     {
-        switch( State )
+        switch (State)
         {
         case RX:
-            if( isMaster == true )
+            if (isMaster == true)
             {
-                if( BufferSize > 0 )
+                if (BufferSize > 0)
                 {
-                    if( strncmp( ( const char* )Buffer, ( const char* )PongMsg, 4 ) == 0 )
+                    if (strncmp((const char *)Buffer, (const char *)PongMsg, 4) == 0)
                     {
                         // Indicates on a LED that the received frame is a PONG
-                        GpioToggle( &Led1 );
+                        GpioToggle(&Led1);
 
                         // Send the next PING frame
                         Buffer[0] = 'P';
@@ -279,35 +296,35 @@ int main( void )
                         Buffer[2] = 'N';
                         Buffer[3] = 'G';
                         // We fill the buffer with numbers for the payload
-                        for( i = 4; i < BufferSize; i++ )
+                        for (i = 4; i < BufferSize; i++)
                         {
                             Buffer[i] = i - 4;
                         }
-                        DelayMs( 1 );
+                        DelayMs(1);
                         printf("PINGED\n\r");
-                        Radio.Send( Buffer, BufferSize );
+                        Radio.Send(Buffer, BufferSize);
                     }
-                    else if( strncmp( ( const char* )Buffer, ( const char* )PingMsg, 4 ) == 0 )
+                    else if (strncmp((const char *)Buffer, (const char *)PingMsg, 4) == 0)
                     { // A master already exists then become a slave
                         isMaster = false;
-                        GpioToggle( &Led2 ); // Set LED off
-                        Radio.Rx( RX_TIMEOUT_VALUE );
+                        GpioToggle(&Led2); // Set LED off
+                        Radio.Rx(RX_TIMEOUT_VALUE);
                     }
                     else // valid reception but neither a PING or a PONG message
                     {    // Set device as master ans start again
                         isMaster = true;
-                        Radio.Rx( RX_TIMEOUT_VALUE );
+                        Radio.Rx(RX_TIMEOUT_VALUE);
                     }
                 }
             }
             else
             {
-                if( BufferSize > 0 )
+                if (BufferSize > 0)
                 {
-                    if( strncmp( ( const char* )Buffer, ( const char* )PingMsg, 4 ) == 0 )
+                    if (strncmp((const char *)Buffer, (const char *)PingMsg, 4) == 0)
                     {
                         // Indicates on a LED that the received frame is a PING
-                        GpioToggle( &Led1 );
+                        GpioToggle(&Led1);
 
                         // Send the reply to the PONG string
                         Buffer[0] = 'P';
@@ -315,18 +332,18 @@ int main( void )
                         Buffer[2] = 'N';
                         Buffer[3] = 'G';
                         // We fill the buffer with numbers for the payload
-                        for( i = 4; i < BufferSize; i++ )
+                        for (i = 4; i < BufferSize; i++)
                         {
                             Buffer[i] = i - 4;
                         }
-                        DelayMs( 1 );
-                        Radio.Send( Buffer, BufferSize );
+                        DelayMs(1);
+                        Radio.Send(Buffer, BufferSize);
                         printf("PONGED\n\r");
                     }
                     else // valid reception but not a PING as expected
                     {    // Set device as master and start again
                         isMaster = true;
-                        Radio.Rx( RX_TIMEOUT_VALUE );
+                        Radio.Rx(RX_TIMEOUT_VALUE);
                     }
                 }
             }
@@ -335,37 +352,37 @@ int main( void )
         case TX:
             // Indicates on a LED that we have sent a PING [Master]
             // Indicates on a LED that we have sent a PONG [Slave]
-            GpioToggle( &Led2 );
-            Radio.Rx( RX_TIMEOUT_VALUE );
+            GpioToggle(&Led2);
+            Radio.Rx(RX_TIMEOUT_VALUE);
             printf("TX\n\r");
             State = LOWPOWER;
             break;
         case RX_TIMEOUT:
-            printf("TX timeout\n\r");
         case RX_ERROR:
-            if( isMaster == true )
+            printf("TX error or timeout\n\r");
+            if (isMaster == true)
             {
                 // Send the next PING frame
                 Buffer[0] = 'P';
                 Buffer[1] = 'I';
                 Buffer[2] = 'N';
                 Buffer[3] = 'G';
-                for( i = 4; i < BufferSize; i++ )
+                for (i = 4; i < BufferSize; i++)
                 {
                     Buffer[i] = i - 4;
                 }
-                DelayMs( 1 );
-                Radio.Send( Buffer, BufferSize );
+                DelayMs(1);
+                Radio.Send(Buffer, BufferSize);
             }
             else
             {
-                Radio.Rx( RX_TIMEOUT_VALUE );
+                Radio.Rx(RX_TIMEOUT_VALUE);
             }
             State = LOWPOWER;
             break;
         case TX_TIMEOUT:
             printf("tx\n");
-            Radio.Rx( RX_TIMEOUT_VALUE );
+            Radio.Rx(RX_TIMEOUT_VALUE);
             State = LOWPOWER;
             break;
         case LOWPOWER:
@@ -374,50 +391,50 @@ int main( void )
             break;
         }
 
-        BoardLowPowerHandler( );
+        BoardLowPowerHandler();
         // Process Radio IRQ
-        if( Radio.IrqProcess != NULL )
+        if (Radio.IrqProcess != NULL)
         {
-            Radio.IrqProcess( );
+            Radio.IrqProcess();
         }
     }
 }
 
-void OnTxDone( void )
+void OnTxDone(void)
 {
     printf("tx done\n");
-    Radio.Sleep( );
+    Radio.Sleep();
     State = TX;
 }
 
-void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr )
+void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr)
 {
     printf("rx done\n");
-    Radio.Sleep( );
+    Radio.Sleep();
     BufferSize = size;
-    memcpy( Buffer, payload, BufferSize );
+    memcpy(Buffer, payload, BufferSize);
     RssiValue = rssi;
     SnrValue = snr;
     State = RX;
 }
 
-void OnTxTimeout( void )
+void OnTxTimeout(void)
 {
     printf("tx timeout\n");
-    Radio.Sleep( );
+    Radio.Sleep();
     State = TX_TIMEOUT;
 }
 
-void OnRxTimeout( void )
+void OnRxTimeout(void)
 {
     printf("rx timeout\n");
-    Radio.Sleep( );
+    Radio.Sleep();
     State = RX_TIMEOUT;
 }
 
-void OnRxError( void )
+void OnRxError(void)
 {
     printf("error\n");
-    Radio.Sleep( );
+    Radio.Sleep();
     State = RX_ERROR;
 }
